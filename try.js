d161b55b59aeab81fe0061734a4b72be8fb4ce54
JORAWARSINGHSAINI <jorawarsingh12@gmail.com>
2020-12-03 00:11:19 +0530

    removed bugs and simplified code

--- try.js
+++ try.js
@@ -1,26 +1,44 @@
 var time = document.querySelector("#time");
- var events=new Array(31);
-for(var i=0;i<31;i++)
-    events[i]="";
 var image_arr=["https://images.unsplash.com/photo-1420593248178-d88870618ca0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","https://cdn.sharechat.com/beautifulnature_882bb8d0-a2c6-4bf1-9c4e-e9e0fec6fa8e-34f3619d-9efe-42ee-9f08-c9a780fb7451_cmprsd_40.jpg","https://cdnaws.sharechat.com/1534585837859_284_compressed_40.jpg","https://cdn-ak.sharechat.com/%EF%B8%8Fnaturebeauty%EF%B8%8F_84857dea-ce01-4094-a326-e7f6514c152e-70d6f1c2-c3e4-41ef-a628-5ae1d72c33d7_cmprsd_40.jpg","https://cdnaws.sharechat.com/73316340-7264-491f-8558-07365de90fab-28ea6ae2-adbe-4731-82e4-e16bdf404953_compressed_40.jpg"];
-var d1 = new Date();
-var curr_day=d1.getDate();
-var month=d1.getMonth(); //curr month;
-var year=d1.getFullYear();
-var after=document.querySelector("#after");
-    create_calendar();
+var events = new Array();
+var date,month,year;
+
+function initialize()
+{
+    var d1 = new Date();
+    console.log(d1)
+    date=d1.getDate();
+    month=d1.getMonth(); 
+    year=d1.getFullYear();
+}
+initialize();
+create_calendar();
+// adding for getting next month
+
+var after=document.querySelector("#after");    
 after.addEventListener("click",function() {
     month+=1;
-    d1=new Date(year,month,1);
-    create_calendar();
-});
-var before=document.querySelector("#before");
-before.addEventListener("click",function() {
-    month-=1;
-    d1=new Date(year,month,1);
+    if(month>11)
+    {
+        month = 0;
+        year+=1;
+    }
     create_calendar();
 });
 
+// adding for getting previous month
+var before=document.querySelector("#before");
+before.addEventListener("click",function() {
+    month-=1;
+    if(month < 0)
+    {
+        month = 11;
+        year-=1;
+    };
+    create_calendar();
+});
+
+//setting time to show
 setInterval(function(){
     var d = new Date();
 
@@ -44,95 +62,136 @@ else
     time.innerHTML+=" PM";    
 },100);
 
-function create_calendar(){
+function set_image()
+{
     //set image from array
     var image=document.querySelector("#photo");
     var value=String(image_arr[Math.floor(4*Math.random())]);
-    console.log(value);
-  image.style.backgroundImage="url("+value+")";
-var d2=new Date(year,month,1);
-var day=d2.getDay();
-var x=document.querySelector(".day");
-var margin=String(day*52)+"px";
-x.style.marginLeft=margin;
-        document.querySelectorAll(".day")[28].style.visibility="visible";
-        document.querySelectorAll(".day")[29].style.visibility="visible";
-        document.querySelectorAll(".day")[30].style.visibility="visible";
-//month of feb
-if(month===1)
-    {
-        
-        document.querySelectorAll(".day")[28].style.visibility="hidden";
-        document.querySelectorAll(".day")[29].style.visibility="hidden";
-        document.querySelectorAll(".day")[30].style.visibility="hidden";
-    }
-//leap year
-if((year%400)===0 ||((year%4)===0) && (year%100!==0))
-    {
-        document.querySelectorAll(".day")[28].style.visibility="visible";
-    }
-   //30day month
-if((month===3) || (month===5) ||(month===8) ||(month===10) ){
-   document.querySelectorAll(".day")[30].style.visibility="hidden";
-   }
-// mark sundays red and other black;
-for(var i=1;i<=31;i=i+1){
-    var x=document.querySelectorAll(".day")[i-1];
-            x.style.color="black";
-            x.style.backgroundColor="azure";
-        if(events[i-1]!=="")
-            x.style.backgroundColor="darkorange";
-}    
-   var y;
-for(var i=1;i<=7;i++)
+    image.style.backgroundImage="url("+value+")";
+}
+function set_sunday_red()
 {
-    var d3=new Date(year,month,i);
-    if(d3.getDay()==0)
-        {
-            y=d3.getDate();
-            break;
-        }
+    // others black
+    for(var i=1;i<=31;i=i+1){
+        var x=document.querySelectorAll(".day")[i-1];
+                x.style.color="black";
+                x.style.backgroundColor="azure";
+        
+    } 
+
+    // all sundays red;
+    var y;
+    for(var i=1;i<=7;i++)
+    {
+        var d3=new Date(year,month,i);
+        if(d3.getDay()==0)
+            {
+                y=d3.getDate();
+                break;
+            }
+    }
+    for(var i=y;i<=31;i=i+7){
+        var x=document.querySelectorAll(".day")[i-1];
+        if(x!=="undefined")
+            {
+                x.style.color="red";
+            }
+    }
+       
 }
 
-// all sundays red;
-for(var i=y;i<=31;i=i+7){
-    var x=document.querySelectorAll(".day")[i-1];
-    if(x!=="undefined")
+function set_events_color()
+{
+    for(var i = 0;i<events.length;i+=1)
+    {
+        var tdate,tmonth,tyear;
+        tdate = events[i].getDate();
+        tmonth = events[i].getMonth();
+        tyear = events[i].getFullYear();
+        if(tmonth === month && tyear === year)
         {
-            x.style.color="red";
+                var x=document.querySelectorAll(".day")[tdate-1];
+                x.style.color="green";
+                x.style.backgroundColor="orange";   
         }
+    }
+}
+
+function create_calendar(){
+    
+    
+    var temp = new Date(year,month,1);
+    var day=temp.getDay();
+    var x=document.querySelector(".day");
+    var margin=String(day*52)+"px";
+    x.style.marginLeft=margin;
+
+    var complete_info=document.querySelector("#complete_info");
+    complete_info.innerHTML = "Month ->"+(month+1)+"  Year -> "+year;
+
+// else required as for next month they should be vissible
+if(month === 1)
+{   
+    document.querySelectorAll(".day")[28].style.visibility="hidden";
+    document.querySelectorAll(".day")[29].style.visibility="hidden";
+    document.querySelectorAll(".day")[30].style.visibility="hidden";
+}
+else
+{   
+    document.querySelectorAll(".day")[28].style.visibility="visible";
+    document.querySelectorAll(".day")[29].style.visibility="visible";
+    document.querySelectorAll(".day")[30].style.visibility="visible";
+}
+//leap year
+if((year%400)===0 ||((year%4)===0) && (year%100!==0))
+{
+     document.querySelectorAll(".day")[28].style.visibility="visible";
+}
+   //30day month
+if((month=== 1) || (month=== 3) || (month===5) || (month===8) ||(month===10) ){
+   document.querySelectorAll(".day")[30].style.visibility="hidden";
+   }
+else{
+    document.querySelectorAll(".day")[30].style.visibility="visible";
 }
 
 //current day black with white text;
 var d4=new Date();
 var days=document.querySelector("#days");
-    if(month===d4.getMonth()){
-days.children[curr_day-1].style.color="white";
-days.children[curr_day-1].style.backgroundColor="aquamarine";
+    if(month === d4.getMonth()){
+        days.children[date-1].style.color="white";
+        days.children[date-1].style.backgroundColor="aquamarine";
     }
+    set_image();
+    set_sunday_red();
+    set_events_color();
 }
-   
-//addevent
-function create_event(i,type_event){
-    document.querySelectorAll(".day")[i-1].classList.add("event");
-     
-    console.log(events);
-    if( type_event!=null){
-        events[i-1]=type_event;
-        document.querySelectorAll(".day")[i-1].style.backgroundColor="darkorange";
-    }
-    else{
-    if(events[i-1]!==null && events[i-1]!=="")
-        alert(events[i-1]);
-    else{
-        events[i-1]=prompt("Enter event name");
-        if(events[i-1]!=="")
-        document.querySelectorAll(".day")[i-1].style.backgroundColor="darkorange";
-    }
+  //addevent
+function create_event(tdate,type_event){
+
         
-    }
-        
-}
+        var flag=-1;
+        var t = new Date(year,month,tdate);
+        for(var i = 0;i<events.length;i+=1)
+        {
+            if(events[i].getDate() === t.getDate() && events[i].getMonth() === t.getMonth() && events[i].getFullYear() === t.getFullYear())
+            {
+                flag = events[i];
+                break;
+            }
+
+        }
+        if(flag !== -1)
+        {
+            alert(flag);   
+        }
+        else{ 
+            events.push(t);
+            create_calendar();
+        }
+       
+} 
+
 //speech recognition
 var microphone=document.querySelector(".microphone");
 microphone.addEventListener("click",function(){
@@ -178,7 +237,7 @@ microphone.addEventListener("click",function(){
     }
     recognition.onresult=function(event){
         var x=event.results[0][0].transcript;
-        console.log(x);//prints transcript
+        microphone.classList.remove("record");
         for(property in operations)
                 {
                    x= x.replace(property,operations[property]);
@@ -188,42 +247,30 @@ microphone.addEventListener("click",function(){
         var date_event,add_event=0,type_event=null;
         for(var i=0;i<arr.length;i++)
             {
-                if(arr[i]=="add"){
-                    add_event=1;
-                }
                 if(!isNaN(arr[i])){
                     date_event=Number(arr[i]);
                 }
-                if(arr[i]=="birthday" || arr[i]=="meeting" ){
-                   type_event=arr[i];
-                   }
-                
+                console.log(date_event)
+                create_event(date_event,null);
             }
-        event_to_add(add_event,date_event,type_event)
-        console.log(add_event);
-        microphone.classList.remove("record");
+   
+        
     }
     
 });
 
-function event_to_add(add_event,date_event,type_event)
-{
-    if(add_event==1)
-        {
-            create_event(date_event,type_event);
-        }
-//    else{
-//        delete_event(date_event);
-//    }
-}
 
+//for dark mode
 function dark_mode_event(){
     var body=document.querySelector("body");
    var x=body.style.backgroundColor;
     if(x==="azure" || x===""){    document.querySelector("#text").style.color="white";
         body.style.backgroundColor="#1a1a1a"
+        document.querySelector("#complete_info").style.color="white";
                    }
-    else{    document.querySelector("#text").style.color="black";
+    else{    
+        document.querySelector("#text").style.color="black";
         body.style.backgroundColor="azure";
-                   }
+        document.querySelector("#complete_info").style.color="black";
+         }
 }
